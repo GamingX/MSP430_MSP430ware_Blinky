d18675c37b98f487ec6916e4a86a09ea100d4d2a
GamingX <gamingx2005@gmail.com>
2015-03-05 21:00:39 -0600

    Final commit, commented code with LED successfully blinking every second.

--- main.c
+++ main.c
@@ -10,9 +10,9 @@ int main(void) {
 	GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0); //Setting P1.0 as Output Pin
 	
 	while(1){
-		GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
-		_delay_cycles(ONE_SECOND);
-		GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+		GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0); // Sets P1.0 as high, LED glows
+		_delay_cycles(ONE_SECOND);						  // In-built delay function for one second
+		GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);  // Resets P1.0, LED turns off
 		_delay_cycles(ONE_SECOND);
 	}
 	return 0;
