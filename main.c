090e4a4b59475bbc9c144888340696b8f05233d4
GamingX <gamingx2005@gmail.com>
2015-03-05 21:44:10 -0600

    Modified the blinky code to read input from a switch, and lighting up the LED to turn on when the switch is pressed.

--- main.c
+++ main.c
@@ -1,19 +1,38 @@
 #include <driverlib.h>
 #include <msp430.h>
 
-#define ONE_SECOND 800000
+//#define ONE_SECOND 800000
 /*
  * main.c
  */
 int main(void) {
-    WDT_A_hold(WDT_A_BASE);	// Stop watchdog timer
-	GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0); //Setting P1.0 as Output Pin
+
+	volatile unsigned short usiButton1 = 0;
+
+	WDT_A_hold(WDT_A_BASE);	// Stop watchdog timer
 	
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0); //Setting P1.0 as Output Pin
+    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1); //Setting P1.1 as Input Pin With Pull Down
+
 	while(1){
-		GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0); // Sets P1.0 as high, LED glows
+
+		usiButton1 = GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN1);
+
+		if (usiButton1 == GPIO_INPUT_PIN_LOW){
+			//if button is down, turn on LED
+			GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
+		}
+		else {
+			//if button is up, turn off LED
+			GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+		}
+
+		}
+
+		/*GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0); // Sets P1.0 as high, LED glows
 		_delay_cycles(ONE_SECOND);						  // In-built delay function for one second
 		GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);  // Resets P1.0, LED turns off
-		_delay_cycles(ONE_SECOND);
-	}
+		_delay_cycles(ONE_SECOND); */
 	return 0;
+
 }
